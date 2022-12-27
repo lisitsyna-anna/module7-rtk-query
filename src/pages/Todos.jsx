53aaa6ceabc48b36676009dc6e60a7761f32a18a
Anna Lisitsyna <anna.lisitsyna93@gmail.com>
2022-12-27 22:41:45 +0100

    Update Todos.jsx

--- src/pages/Todos.jsx
+++ src/pages/Todos.jsx
@@ -1,9 +1,6 @@
 import { Link } from 'react-router-dom';
 import { TodoList } from 'components/TodoList/TodoList';
-import {
-  useFetchTodosQuery,
-  useDeleteTodoMutation,
-} from 'redux/todos/todoSlice';
+import { useFetchTodosQuery } from 'redux/todos/todoSlice';
 import { Spinner } from 'components/Spinner/Spinner';
 
 const TodosPage = () => {
